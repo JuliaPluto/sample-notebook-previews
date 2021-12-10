5cc3d13e092d5e8f85ce900176caccc1293fc02a
Fons van der Plas <fonsvdplas@gmail.com>
2021-12-10 22:17:38 +0100

    🐶

--- generate.jl
+++ generate.jl
@@ -17,14 +17,16 @@ samples_dir = Pluto.project_relative_path("sample")
 
 cp(samples_dir, input; force = true)
 
+exclude = [
+    "Basic.jl"
+    "old_notebook_with_using.jl"
+    "test1.jl"
+    "test_embed_display.jl"
+    "test_pkg_bubble.jl"
+]
 PlutoSliderServer.export_directory(input;
-    Export_exclude = [
-        "Basic.jl"
-        "old_notebook_with_using.jl"
-        "test1.jl"
-        "test_embed_display.jl"
-        "test_pkg_bubble.jl"
-    ],
+    Export_exclude = exclude,
+    SliderServer_exclude = exclude,
     Export_output_dir = output,
     Export_baked_state = false,
     Export_baked_notebookfile = false
