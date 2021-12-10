9e93f04ea112153c6c63fd2abe7ae0c9f4aa9011
Fons van der Plas <fonsvdplas@gmail.com>
2021-12-10 22:28:02 +0100

    👀 index.html

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sample-notebook-previews
 HTML exports of Pluto's sample notebooks
+
+Are you interested in generating HTML exports of your notebooks automatically using GitHub Actions? Take a look at our [template repostiory](https://github.com/JuliaPluto/static-export-template)!
--- generate.jl
+++ generate.jl
@@ -11,18 +11,25 @@ input = joinpath(root, "input")
 # isdir(input) || mkdir(input)
 
 
-
+sample_html_path = Pluto.project_relative_path("frontend", "sample.html")
+write(joinpath(output, "index.html"), let
+    original = read(sample_html_path, String)
+    replace(original,
+        """href="sample/""" => """href="./""",
+        ".jl\">" => "\">",
+    )
+end)
 
 samples_dir = Pluto.project_relative_path("sample")
 
 cp(samples_dir, input; force = true)
 
 exclude = [
-    "Basic.jl"
-    "old_notebook_with_using.jl"
-    "test1.jl"
-    "test_embed_display.jl"
-    "test_pkg_bubble.jl"
+# "Basic.jl"
+# "old_notebook_with_using.jl"
+# "test1.jl"
+# "test_embed_display.jl"
+# "test_pkg_bubble.jl"
 ]
 PlutoSliderServer.export_directory(input;
     Export_exclude = exclude,
